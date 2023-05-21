8748a9c268cca8fd829a0cb4e41a3e316f2675fa
Daanturo <daanturo@gmail.com>
2023-05-21 23:44:57 +0700

    Update against cookies requirement

--- hugging-chat-comint.py
+++ hugging-chat-comint.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python
 
 import argparse
+import json
 
-import hugchat
+try:
+    from hugchat import hugchat
+except Exception:
+    import hugchat
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("--model")
+arg_parser = argparse.ArgumentParser(
+    description="See https://github.com/Soulter/hugging-chat-api"
+)
 
-args = parser.parse_args()
+arg_parser.add_argument("cookies", help="JSON string or cookies files path")
+arg_parser.add_argument(
+    "-p",
+    "--prompt",
+    default=">>>> ",
+    help='Prompt indicator, a space is ensured at the end, defaults to ">>>> "',
+)
 
-hugchat.cli(args=args, hello=True)
+parsed_args = arg_parser.parse_args()
+
+try:
+    cookies_dict = json.loads(parsed_args.cookies)
+except ValueError:
+    cookies_dict = None
+
+if cookies_dict:
+    chatbot = hugchat.ChatBot(cookies=cookies_dict)
+else:
+    chatbot = hugchat.ChatBot(cookie_path=parsed_args.cookies)
+
+prompt_indicator = parsed_args.prompt
+if prompt_indicator[-1] != " ":
+    prompt_indicator += " "
+
+
+while True:
+    try:
+        prompt = input(prompt_indicator)
+        if prompt != "":
+            print(chatbot.chat(prompt))
+    except EOFError:
+        break
